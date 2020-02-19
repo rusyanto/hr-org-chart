4527d64d48a3d5a30f11924d6320a110bbffd315
Rusyanto Rusanto <rusyanto@gmail.com>
2020-02-19 09:32:08 +0800

    Added example org chart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
-import './App.css';
+import { Chart } from 'react-google-charts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"my-pretty-chart-container"}>
+      <Chart
+        width={'100%'}
+        height={350}
+        chartType="OrgChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['Name', 'Manager', 'ToolTip'],
+          [
+            {
+              v: 'Mike',
+              f: 'Mike<div style="color:red; font-style:italic">President</div>',
+            },
+            '',
+            'The President',
+          ],
+          [
+            {
+              v: 'Jim',
+              f:
+                'Jim<div style="color:red; font-style:italic">Vice President</div>',
+            },
+            'Mike',
+            'VP',
+          ],
+          ['Alice', 'Mike', ''],
+          ['Bob', 'Jim', 'Bob Sponge'],
+          ['Carol', 'Bob', ''],
+        ]}
+        options={{
+          allowHtml: true,
+          allowCollapse: true
+        }}
+        rootProps={{ 'data-testid': '1' }}
+      />
     </div>
   );
 }
